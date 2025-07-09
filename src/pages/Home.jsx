b3a07d94bf6c06e2c725263f430409eacf4f46a7
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-07-09 19:07:30 -0400

    Changes made to add Contacts Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { AboutSection } from "../components/AboutSection";
+import { ContactSection } from "../components/ContactSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectsSection";
@@ -22,6 +23,7 @@ export const Home = () => {
             <AboutSection/>
             <SkillsSection/>
             <ProjectsSection/>
+            <ContactSection/>
         </main>
         {/*Footer*/}
 
