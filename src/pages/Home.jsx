1360900fe28fcaa8d31c0b08f5ebf24b815b0f3e
Sravan kumar Kurapati <sravankumarkurapati@Sravans-MacBook-Air.local>
2025-07-09 18:27:33 -0400

    Changed made to add Skills Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
+import { AboutSection } from "../components/AboutSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -16,6 +18,8 @@ export const Home = () => {
         {/* Main Content */}
         <main>
             <HeroSection/>
+            <AboutSection/>
+            <SkillsSection/>
         </main>
         {/*Footer*/}
 
